9a763aca0feae82a5cce3d719549949d1ab474f1
Manuel <Manusolerj@gmail.com>
2025-07-07 13:55:00 +0200

    Feat: Implement Move entity with attributes and MoveCategory enum for categorization

---
+++ src/main/java/com/pkm/entities/Move.java
@@ -0,0 +1,45 @@
+package com.pkm.entities;
+
+import com.pkm.utils.enums.MoveCategory;
+
+import jakarta.persistence.*;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "moves")
+@Data
+public class Move {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true, insertable = false, updatable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true, length = 30)
+    private String name;
+
+    @Column(name = "effect", nullable = false)
+    @Lob
+    private String effect;
+
+    @JoinColumn(name = "type")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Type type;
+
+    @Column(name = "power")
+    private int power;
+
+    @Column(name = "accuracy")
+    private int accuracy;
+
+    @Column(name = "pp")
+    private int pp;
+
+    @Column(name = "priority")
+    private int priority = 0;
+
+    @Column(name = "category")
+    @Enumerated(EnumType.STRING)
+    private MoveCategory category;
+}
---
+++ src/main/java/com/pkm/utils/enums/MoveCategory.java
@@ -0,0 +1,27 @@
+package com.pkm.utils.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum MoveCategory {
+    PHYSICAL("physical"),
+    SPECIAL("special"),
+    STATUS("status");
+
+    private final String category;
+
+    MoveCategory(String category){
+        this.category = category;
+    }
+
+    public static MoveCategory fromString(String category){
+        if(category != null){
+            for(MoveCategory moveCategory : MoveCategory.values()){
+                if(moveCategory.category.equals(category) || moveCategory.name().equals(category)){
+                    return moveCategory;
+                }
+            }
+        }
+        throw new IllegalArgumentException("Invalid category: " + category);
+    }
+}
