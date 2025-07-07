8c68d7c0d936ffe6cd08e8a95770587a4c8b8c4d
Manuel <Manusolerj@gmail.com>
2025-07-07 19:38:16 +0200

    Feat:
    - Implemented remaining fields.

--- src/main/java/com/pkm/entities/UserPokemon.java
+++ src/main/java/com/pkm/entities/UserPokemon.java
@@ -48,4 +48,46 @@ public class UserPokemon {
         inverseJoinColumns = @JoinColumn(name = "move_id")
     )
     private Set<Move> moves = new HashSet<>();
+
+    @Column(name = "isShiny", nullable = false)
+    private boolean shiny = false;
+
+    @Column(name = "level", nullable = false)
+    private int level = 50;
+
+    @Column(name = "hp_ev")
+    private int hpEV;
+
+    @Column(name = "attack_ev")
+    private int attackEV;
+
+    @Column(name = "defense_ev")
+    private int defenseEV;
+
+    @Column(name = "sp_attack_ev")
+    private int spAttackEV;
+
+    @Column(name = "sp_defense_ev")
+    private int spDefenseEV;
+
+    @Column(name = "speed_ev")
+    private int speedEV;
+
+    @Column(name = "hp_iv")
+    private int hpIV;
+
+    @Column(name = "attack_iv")
+    private int attackIV;
+
+    @Column(name = "defense_iv")
+    private int defenseIV;
+
+    @Column(name = "sp_attack_iv")
+    private int spAttackIV;
+
+    @Column(name = "sp_defense_iv")
+    private int spDefenseIV;
+
+    @Column(name = "speed_iv")
+    private int speedIV;
 }
