1f5d6d033bb500449ea6d1091deac5751d66001c
Manuel <Manusolerj@gmail.com>
2025-07-09 23:08:42 +0200

    Feat: Update UserRepository to use optional on individual searches and pagination support on multiple row search

--- src/main/java/com/pkm/repositories/UserRepository.java
+++ src/main/java/com/pkm/repositories/UserRepository.java
@@ -2,12 +2,27 @@ package com.pkm.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 import com.pkm.entities.User;
+import com.pkm.utils.enums.UserRole;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
     
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 
+    Optional<User> findByUsernameIgnoreCase(String username);
+    
+    Page<User> findAllByUsernameContainingIgnoreCase(String username, Pageable pageable);
 
+    Page<User> findAllByCreatedAtAfter(LocalDateTime createdAt, Pageable pageable);
+
+    Page<User> findAllByUpdatedAtAfter(LocalDateTime updatedAt, Pageable pageable);
+
+    Page<User> findAllByRole(UserRole role, Pageable pageable);
 }
