7d48ad38c3c6ed2a221bfa7af1d51bc642fc315b
Manu <manusolerj@gmail.com>
2026-04-13 20:39:36 +0200

    Refactor: Improved method to detect lack of criteria in filter

--- src/main/java/com/poketeambuilder/interfaces/FilterDtoInterface.java
+++ src/main/java/com/poketeambuilder/interfaces/FilterDtoInterface.java
@@ -1,6 +1,21 @@
 package com.poketeambuilder.interfaces;
 
+import java.lang.reflect.Field;
+
 public interface FilterDtoInterface {
     
-    boolean hasAnyCriteria();
+    default boolean hasAnyCriteria() {
+        for (Field field : getClass().getDeclaredFields()) {
+            field.setAccessible(true);
+
+            try {
+                if (field.get(this) != null){
+                    return true;
+                }
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return false;
+    }
 }
