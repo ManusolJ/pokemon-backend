16293f250bd49f49db82410d7caff1541332aa45
Manu <manusolerj@gmail.com>
2026-04-14 14:22:07 +0200

    Chore: Added changes that were supposed to be added in previous commit...

--- src/main/java/com/poketeambuilder/repositories/PokemonSpeciesEggGroupRepository.java
+++
@@ -1,8 +0,0 @@
-package com.poketeambuilder.repositories;
-
-import com.poketeambuilder.entities.PokemonSpeciesEggGroup;
-import com.poketeambuilder.entities.compositeIDs.PokemonSpeciesEggGroupId;
-
-public interface PokemonSpeciesEggGroupRepository extends BaseRepository<PokemonSpeciesEggGroup, PokemonSpeciesEggGroupId> {
-    
-}
