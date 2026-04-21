8c630e7a31e0b84c9cb2bbf076298e5dfbd96f13
Manu <manusolerj@gmail.com>
2026-04-21 10:59:11 +0200

    fix: Update RegisterDto to ensure proper validation annotations for username and email fields

--- src/main/java/com/poketeambuilder/dtos/auth/RegisterDto.java
+++ src/main/java/com/poketeambuilder/dtos/auth/RegisterDto.java
@@ -1,18 +1,24 @@
 package com.poketeambuilder.dtos.auth;
 
-import lombok.Getter;
+import com.poketeambuilder.utils.validation.annotations.PasswordMatch;
 
 import jakarta.validation.constraints.Size;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
+import lombok.Getter;
+
 @Getter
+@PasswordMatch
 public class RegisterDto {
 
     @NotBlank
+    @Size(min = 3, message = "Username must be at least 3 characters long")
     private String username;
 
     @Email
+    @NotBlank
     private String email;
 
     @NotBlank
