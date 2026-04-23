cf3327998d7871141189a0d9f3d5b8ae47fe4246
Manu <manusolerj@gmail.com>
2026-04-23 12:40:24 +0200

    Chore: Minor aesthetic changes for better readability

--- src/main/java/com/poketeambuilder/dtos/pokeapi/item/ItemSprite.java
+++ src/main/java/com/poketeambuilder/dtos/pokeapi/item/ItemSprite.java
@@ -1,7 +1,7 @@
 package com.poketeambuilder.dtos.pokeapi.item;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record ItemSprite(@JsonProperty("default") String defaultSprite) {
