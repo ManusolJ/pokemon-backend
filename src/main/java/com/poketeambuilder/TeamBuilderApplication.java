90dc69c9639d1f78e2b11d1b099804275b930afa
Manu <manusolerj@gmail.com>
2026-05-08 19:42:43 +0200

    Chore: Enabled page serialization via dto for future compatibility

--- src/main/java/com/poketeambuilder/TeamBuilderApplication.java
+++ src/main/java/com/poketeambuilder/TeamBuilderApplication.java
@@ -3,13 +3,19 @@ package com.poketeambuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
 import org.springframework.cache.annotation.EnableCaching;
+
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
+
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableCaching
 @EnableScheduling
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 public class TeamBuilderApplication {
 
 	public static void main(String[] args) {
