2e388bde5212db235bfba1db3496e531ee64f71c
Manu <manusolerj@gmail.com>
2026-05-22 16:21:35 +0200

    Feat: Add method to retrieve all move IDs for efficient membership checks

--- src/main/java/com/poketeambuilder/repositories/MoveRepository.java
+++ src/main/java/com/poketeambuilder/repositories/MoveRepository.java
@@ -1,8 +1,18 @@
 package com.poketeambuilder.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+
 import com.poketeambuilder.entities.Move;
 
 /** CRUD + specification queries for {@link Move} reference data. */
 public interface MoveRepository extends BaseRepository<Move, Integer> {
-    
+
+    /**
+     * Returns just the ids of every persisted move. Used by the seed pipeline to do a
+     * cheap "is this move id known?" membership check without loading entities.
+     */
+    @Query("SELECT m.id FROM Move m")
+    List<Integer> findAllIds();
 }
